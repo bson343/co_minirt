2d6ead92c4127cd9cff932cd3a1bb9989bb84028
RodelCC <byeonggukson@gmail.com>
2022-05-26 15:50:27 +0900

    move srcs/ -> srcs/parsing

---
+++ srcs/parsing/occur_error.c
@@ -0,0 +1,30 @@
+#include "minirt.h"
+
+void	ft_error(char *error_detail)
+{
+	t_minirt	*rt;
+
+	rt = my_minirt(NULL);
+	if (rt != NULL)
+		all_free(rt->image, rt->cam, rt->objs, rt->lights);
+	ft_assert(FALSE, error_detail);
+}
+
+void	*ft_calloc_assert(size_t count, size_t size)
+{
+	void	*temp;
+
+	temp = malloc(size * count);
+	ft_assert(temp != (void *)0, "Out of memory in ft_calloc()");
+	ft_bzero(temp, (size * count));
+	return (temp);
+}
+
+t_minirt	*my_minirt(t_minirt *ptr)
+{
+	static t_minirt	*s_data;
+
+	if (ptr != NULL)
+		s_data = ptr;
+	return (s_data);
+}
